8c616d574896a13c70530e0fbb08d239eb65fbf5
Andreia Dourado <deiadourado93@gmail.com>
2023-08-14 12:47:18 -0400

    update 4 files and delete 1 file

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Golden Record Players</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+    <header>
+    <?php
+        include("includes/global-nav.php");
+    ?>
+    </header>
+    <main>
+        <!-- Page content goes here -->
+    </main>
+    <footer>
+        <p>&copy; 2023 Vintage Vinyl Records</p>
+    </footer>
+</body>
+</html>
