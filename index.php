cf8473db34a216b61b1d3abafc71132452e2a713
Andreia Dourado <deiadourado93@gmail.com>
2023-08-14 21:37:09 -0400

    delete 4 files and update 3 files

--- index.php
+++ index.php
@@ -14,9 +14,26 @@
     </header>
     <main>
         <!-- Page content goes here -->
+        <script src="script.js"></script>
+        <section class="product-ad">
+        <h2>Discover the Raveo Vanguard Maple</h2>
+        <img src="https://images.kabum.com.br/produtos/fotos/459374/toca-discos-raveo-vitrola-vanguard-maple-bluetooth-usb-marrom-90-08958_1683137799_g.jpg" alt="Raveo Vanguard Maple">
+        <p>Experience the nostalgia of vinyl with the Raveo Vanguard Maple record player. Its classic design and modern features make it the perfect addition to your home.</p>
+        <button class="show-details-button">Show Details</button>
+        <div class="product-details">
+            <p>Features:</p>
+            <ul>
+                <li>Bluetooth connectivity for wireless streaming</li>
+                <li>USB port for digitizing your vinyl collection</li>
+                <li>High-quality sound with built-in speakers</li>
+            </ul>
+            <p class="price">Price: $89.99 CAD</p>
+            <a class="buy-now-button" href="#">Buy Now</a>
+        </div>
+    </section>
     </main>
     <footer>
-        <p>&copy; 2023 Vintage Vinyl Records</p>
+        <p>&copy; 2023 Golden Record Players</p>
     </footer>
 </body>
 </html>
---
+++ js/script.js
@@ -0,0 +1,9 @@
+<script>
+    // JavaScript code
+    const showDetailsButton = document.querySelector('.show-details-button');
+    const productDetails = document.querySelector('.product-details');
+
+    showDetailsButton.addEventListener('click', () =&gt; {
+        productDetails.classList.toggle('show')}
+    &rbrace;);
+</script>
